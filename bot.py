15fed481748ab65cc63eea82c3add1fe4f204557
Vladimir <vladimiburash@mail.ru>
2017-01-28 19:15:58 +0300

    add groups

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import config
-import telebot,requests,datetime,aiohttp,asyncio,json
+import telebot,datetime,aiohttp,asyncio
 
 
 bot = telebot.TeleBot(config.token)
@@ -34,11 +34,13 @@ async def friends_ages(message):
                 bArr = []
                 while i < count:
                     bdate = req['response']['items'][i].get('bdate')
-
                     if bdate:
                         if len(bdate) > 5:
-                            bdate = datetime.datetime.strptime(bdate, "%d.%m.%Y")
-                            bArr.insert(i, bdate)
+                            try:
+                                bdate = datetime.datetime.strptime(bdate, "%d.%m.%Y")
+                                bArr.insert(i, bdate)
+                            except:
+                                print('1')
                     i += 1
                 diag = [0 for i in range(150)]
                 count1 = len(bArr)
@@ -67,12 +69,85 @@ async def friends_ages(message):
                     i += 1
                 bot.send_message(message.chat.id,' '.join(answer))
 
+async def group_ages(message):
+    offset = 0
+    await loading(message)
+    try:
+        groupname = message.text.split(' ')[1]
+    except:
+        bot.send_message(message.chat.id,'Вы забыли ввести id группы')
+        return 0
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.vk.com/method/groups.getMembers?group_id="+groupname+"&offset="+str(offset)+"&fields=bdate&fields=bdate&v=5.58") as resp:
+            req_id =  await resp.json()
+            count = req_id['response'].get('count')
+            loops= count/1000
+            i = 0
+            bArr=[]
+            while i < loops:
+                async with session.get("https://api.vk.com/method/groups.getMembers?group_id=" + groupname + "&offset=" + str(offset) + "&fields=bdate&fields=bdate&v=5.58") as resp:
+                    j = 0
+                    req = await resp.json()
+                    if count-((i+1)*1000)>0:
+                        con = 1000
+                    else:
+                        con = 1000-abs(count-(i+1)*1000)
+                    while j < con:
+                        bdate = req['response']['items'][j].get('bdate')
+                        if bdate:
+                            if len(bdate) > 5:
+                                try:
+                                    bdate = datetime.datetime.strptime(bdate, "%d.%m.%Y")
+                                    bArr.insert(j, bdate)
+                                    # bot.send_message(message.chat.id,bdate)
+                                except:
+                                    print("wrong")
+                        j += 1
+                i += 1
+                offset += 1000
+            diag = [0 for i in range(120)]
+            count1 = len(bArr)
+            i = 0
+            a = []
+            while i < count1:
+                result = (datetime.datetime.today() - bArr[i]) / 365
+                result = str(result).split(' ')
+                j = int(result[0])
+                diag[j] += 1
+                i += 1
+            answer = [" "]
+            i = 0
+            cnt = len(diag)
+            answer.append("Возраст - Кол-во"+ "\n")
+            while i < cnt:
+                if diag[i] != 0:
+                    answer.append(str(i))
+                    answer.append('    -    ')
+                    answer.append(str(diag[i]))
+                    answer.append("\n")
+
+                i += 1
+            bot.send_message(message.chat.id,' '.join(answer))
+
+
+
+
+
+
 @bot.message_handler(commands=["friends_ages"])
 def loop(message):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     # Blocking call which returns when the display_date() coroutine is done
     loop.run_until_complete(friends_ages(message))
+    loop.close()
+
+@bot.message_handler(commands=["group_ages"])
+def groups(message):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    # Blocking call which returns when the display_date() coroutine is done
+    loop.run_until_complete(group_ages(message))
     loop.close()
 
 if __name__ == '__main__':
